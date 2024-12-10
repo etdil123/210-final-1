c428616affa7350f0441031d4f3cd8c609b3a88c
etdil123 <ethandilk@MacBook-Air.attlocal.net>
2024-12-09 18:43:31 -0800

    10 min commit

--- main.cpp
+++ main.cpp
@@ -1,11 +1,37 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
+struct Node {
+    string customer_name;
+    string drink_order;
+    Node * next;
+};
+
+// Coffee Booth Functions
+// function to insert a Node at the head
+
+// function to insert a Node at the tail
+
+// function to output each customer
+
+// function to delete the entire linked list
 
 int main () {
-
-
+    // array of random names
+    string names[20] = {
+        "Alice", "Bob", "Charlie", "Diana", "Evan",
+        "Fiona", "George", "Hannah", "Ian", "Julia",
+        "Kevin", "Luna", "Mason", "Nora", "Oliver",
+        "Penelope", "Quentin", "Ruby", "Sam", "Tina"
+    };
+    // array of random drinks
+    string drinks[20] = {
+        "Espresso", "Latte", "Cappuccino", "Americano", "Mocha",
+        "Macchiato", "Flat White", "Cold Brew", "Iced Latte", "Frappuccino",
+        "Affogato", "Cortado", "Ristretto", "Irish Coffee", "Turkish Coffee",
+        "Cafe au Lait", "Long Black", "Doppio", "Vienna Coffee", "Red Eye"
+    };
 
 
 
